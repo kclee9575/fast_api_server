585e114d05df526341f103fa04354e0f26d3749b
kclee9575 <lgc1636@naver.com>
2022-10-17 18:58:28 +0900

    [add] auth commit

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -8,12 +8,10 @@ from jose import jwt
 class AuthManager:
     def __init__(self):
         self.ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
-        self.REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 day
+        self.REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 day
         self.ALGORITHM = "HS256"
-        self.JWT_SECRET_KEY = (
-            "test_jwt_key_value"
-        )
-        self.JWT_REFRESH_SECRET_KEY = "test_jwt_refresh_key_value" 
+        self.JWT_SECRET_KEY = "test_jwt_key_value"
+        self.JWT_REFRESH_SECRET_KEY = "test_jwt_refresh_key_value"
         self.password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     def decode_jwt(self, token: str):
